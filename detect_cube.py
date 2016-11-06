8b67d98234d03c8505c3e68539938c29ee905f03
Rachel Lee <slee17@cmc.edu>
2016-11-06 00:05:43 -0700

    added a better function for finding squares that are part of the cube face

--- detect_cube.py
+++ detect_cube.py
@@ -53,13 +53,73 @@ def pick_centers(centers):
 
     return corners
 
+# pick centers by finding points that have a reasonable bounding box
+"""def pick_centers2(centers):
+    if len(centers) <= 3:
+        return centers
+    useit_points = [centers.pop(0)]
+    useit_points.extend(pick_centers2(centers))
+    #print useit_points
+    (x,y,w,h) = cv2.boundingRect(np.array([[p] for p in useit_points]))
+    useit_ratio = w / float(h) 
+    print useit_ratio
+    loseit_points = pick_centers2(centers)
+    #print loseit_points
+    (x,y,w,h) = cv2.boundingRect(np.array([[p] for p in loseit_points]))
+    loseit_ratio = w / float(h)
+
+    thresh = 0.15
+    if useit_ratio > 1.0 - thresh and useit_ratio < 1.0 + thresh:
+        return useit_points
+    return loseit_points
+    """     
+
+def contour_is_square_old(contour):
+    peri = cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(contour, 0.04 * peri, True)
+    
+    # 4 sides
+    if len(approx) == 4 or len(approx) == 5 or len(approx) == 6:
+        # only keep contours that are square/circular enough in nature
+        M = cv2.moments(approx)
+        if M["m00"] == 0:
+            return False
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+
+        distances = []
+        for p in approx:
+            distances.append(math.sqrt((p[0][0] - cX)**2 + (p[0][1] - cY)**2))
+            
+        if np.std(distances) < 1:
+            return True
+    return False
+
+def contour_is_square(contour):
+    peri = cv2.arcLength(contour, True)
+    area = cv2.contourArea(contour)
+    if area == 0:
+        return False
+
+    ap_ratio = (float(peri)/4) / math.sqrt(area)
+    return ap_ratio > 0.8 and ap_ratio < 1.2
+
+def contour_center(contour):
+    peri = cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(contour, 0.04 * peri, True)
+    M = cv2.moments(approx)
+    if M["m00"] == 0:
+        return (float('inf'), float('inf'))
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
+    return (cX, cY)
+
 def find_shapes(img):
     rows,cols,channels = img.shape
-    img_small = cv2.resize(img, (cols/10, rows/10))
+    img_small = cv2.resize(img, (cols/5, rows/5))
     edges = auto_canny(img_small)
     cv2.imwrite('auto_edges.png', edges)
-    edges = cv2.dilate(edges, np.ones((3,3)), iterations=1)
-    #edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, np.ones((2,2)), iterations=1)
+    edges = cv2.dilate(edges, np.ones((4,4)), iterations=1)
     edges = 255-edges
     cv2.imwrite('auto_edges_dilated.png', edges)
     contour_img, contours, hierarchy = cv2.findContours(edges.copy(),\
@@ -69,56 +129,38 @@ def find_shapes(img):
         color = (random.randint(0, 255), random.randint(0,255), random.randint(0,255))
         cv2.drawContours(img_copy, [contour], 0, color, 1)
     cv2.imwrite('contours.png', img_copy)
-
     img_copy = img_small.copy()
-    new_img = np.zeros((rows/10, cols/10, 3))
+    new_img = np.zeros((rows/5, cols/5, 3))
     centers = []
+    start = time.time()
     for contour in contours:
-        peri = cv2.arcLength(contour, True)
-        approx = cv2.approxPolyDP(contour, 0.04 * peri, True)
-
-        #for point in contour:
-        #    if point[0][0] == 59 and point[0][1] == 42:
-        #        print contour
-        #        print approx
-        #        color = (random.randint(0, 255), random.randint(0,255), random.randint(0,255))
-        #        cv2.drawContours(img_copy, [approx], 0, color, -1)
-
-        
-        # 4 sides
-        if len(approx) == 4 or len(approx) == 5 or len(approx) == 6:
-            # only keep contours that are square/circular enough in nature
-            M = cv2.moments(approx)
-            if M["m00"] == 0:
-                continue
-	    cX = int(M["m10"] / M["m00"])
-	    cY = int(M["m01"] / M["m00"])
+        # perimiter to area ratio does a good job of detecting squares, we will
+        # want to find something else for detecting rectangles
+        if contour_is_square(contour):
+            color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+            cv2.drawContours(img_copy, [contour], 0, color, -1)    
+            cX,cY = contour_center(contour)
+            centers.append((cX,cY))
+            cv2.rectangle(new_img, (cX-2, cY-2), (cX+2, cY+2), (255,0,0), -1)
             
-            distances = []
-            for p in approx:
-                distances.append(math.sqrt((p[0][0] - cX)**2 + (p[0][1] - cY)**2))
-
-            if np.std(distances) < 1:
-                color = (random.randint(0, 255), random.randint(0,255), random.randint(0,255))
-                cv2.drawContours(img_copy, [approx], 0, color, -1)
-                cv2.rectangle(new_img, (cX-2, cY-2), (cX+2, cY+2), (255,0,0), -1)
-                centers.append((cX, cY))
-            
-            #(x, y, w, h) = cv2.boundingRect(approx)
-	    #ar = w / float(h)
-            #if ar >= 0.75 and ar <= 1.25:
-            #    color = (random.randint(0, 255), random.randint(0,255), random.randint(0,255))
-            #    cv2.drawContours(img_copy, [contour], 0, color, -1)
-
+    end = time.time()
     corners = pick_centers(centers)
     for corner in corners:
         color = (random.randint(0, 255), random.randint(0,255), random.randint(0,255))
         cv2.line(new_img, corner[0], corner[1], color, 1)
         cv2.line(new_img, corner[1], corner[2], color, 1)
-
+    
+    #centers2 = pick_centers2(centers)
+    #box = cv2.boxPoints(cv2.minAreaRect(np.array([[c] for c in centers2])))
+    #box = np.int0(box)
+    #cv2.drawContours(new_img,[box],0,(255,255,255),1) 
     cv2.imwrite('approximated_squares.png', img_copy)
     cv2.imwrite('approximated_centers.png', new_img)
+    print "took", end - start
 
 if __name__ == '__main__':
     img = cv2.imread(sys.argv[1])
+    start = time.time()
     find_shapes(img)
+    end = time.time()
+    print "took", end - start, "seconds"
