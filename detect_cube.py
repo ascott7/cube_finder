d1fe495c5a0867fc1fe9456a82438752839b900e
Andrew Scott <ascott@hmc.edu>
2016-11-19 17:50:44 -0800

    added logic for determining if we have a grid, more work needed though

--- detect_cube.py
+++ detect_cube.py
@@ -1,6 +1,7 @@
 import cv2, sys, time, random, os
 import numpy as np
-import math
+import math, copy, itertools
+from skimage import feature
 
 # http://www.pyimagesearch.com/2015/04/06/zero-parameter-automatic-canny-edge-detection-with-python-and-opencv/
 def auto_canny(image, sigma=0.33):
@@ -95,16 +96,41 @@ def contour_is_square_old(contour):
             return True
     return False
 
-def contour_is_square(contour):
+def contour_is_square(contour, img_area):
     peri = cv2.arcLength(contour, True)
     area = cv2.contourArea(contour)
+
     # we should probably change these #s to be some fraction of the rows/cols of the input image
-    if area < 36 or area > 500:
+    if float(area)/img_area < 0.002 or float(area)/img_area > 0.01:
         return False
 
     ap_ratio = (float(peri)/4) / math.sqrt(area)
     thresh = 0.3
     return ap_ratio > 1.0-thresh and ap_ratio < 1.0+thresh
+
+def find_bounding_square(contour, img_area):
+    peri = cv2.arcLength(contour, True)
+    area = cv2.contourArea(contour)
+
+    # we should probably change these #s to be some fraction of the rows/cols of the input image
+    if float(area)/img_area < 0.002 or float(area)/img_area > 0.02:
+        return None
+    
+    bounding_rect = cv2.minAreaRect(contour)
+    box = cv2.boxPoints(bounding_rect)
+    box = np.int0(box)
+    bounding_line1 = math.sqrt((box[0][0]-box[1][0])**2 + (box[0][1]-box[1][1])**2)
+    bounding_line2 = math.sqrt((box[1][0]-box[2][0])**2 + (box[1][1]-box[2][1])**2)
+    bounding_area = bounding_line1*bounding_line2
+
+    ap_ratio = (float(peri)/4) / math.sqrt(area)
+    a_br_diff = bounding_area - area#float(bounding_area - area) / bounding_area
+    ap_thresh = 0.3
+    a_br_thresh = 150
+    if abs(1.0 - ap_ratio) < ap_thresh and a_br_diff < a_br_thresh:
+        return box
+    return None
+
 
 def contour_center(contour):
     peri = cv2.arcLength(contour, True)
@@ -116,50 +142,236 @@ def contour_center(contour):
     cY = int(M["m01"] / M["m00"])
     return (cX, cY)
 
+def prune_centers(centers, img_width):
+    while True:
+        num_centers = len(centers)
+        centers = prune_centers_helper(centers, img_width)
+        if num_centers == len(centers):
+            return centers
+    return []
+
+def prune_centers_helper(centers, img_width):
+    dist_thresh = 0.25
+    pruned_centers = []
+    if len(centers) < 4:
+        return []
+
+    for ci in centers:
+        distances = []
+        for cj in centers:
+            distances.append(abs(float(ci[0]) - cj[0]) + abs(float(ci[1]) - cj[1]))
+        distances.sort()
+        # check if there are 3 nearby points
+        if distances[3]/img_width < dist_thresh:
+            pruned_centers.append(ci)
+    return pruned_centers
+
+def group_centers(centers, img_width):
+    if len(centers) <= 1:
+        return centers
+    dist_thresh = 0.12
+    clusters = []
+    for p in centers:
+        found_cluster = False
+        for i in range(len(clusters)):
+            center = clusters[i][0]
+            if math.hypot(center[0] - p[0], center[1] - p[1])/img_width < dist_thresh:
+                clusters[i][1].append(p)
+                clusters[i][0] = np.mean(clusters[i][1], axis=0)
+                found_cluster = True
+                break
+        if not found_cluster:
+            clusters.append([p, [p]])
+
+    # take out the cluster centers
+    clusters = [x[1] for x in clusters]
+    # try to recombine clusters that are now close to one another
+    n_clusters = float('inf')
+    while n_clusters != len(clusters) and len(clusters) > 1:
+        n_clusters = len(clusters)
+        full_break = False
+        for i in range(len(clusters)):
+            if full_break:
+                break
+            for j in range(i+1, len(clusters)):
+                if full_break:
+                    break
+                for c1 in clusters[i]:
+                    if full_break:
+                        break
+                    for c2 in clusters[j]:
+                        # if 2 clusters are close to one another, merge them
+                        if math.hypot(c1[0] - c2[0], c1[1] - c2[1])/img_width < dist_thresh:
+                            clusters[i].extend(copy.deepcopy(clusters[j]))
+                            clusters.pop(j)
+                            full_break = True
+                            break
+    return clusters
+
+def tilted_grid_score(points):
+    x_vals = [p[0] for p in points]
+    y_vals = [p[1] for p in points]
+    min_y = min(y_vals)
+    min_y_index = y_vals.index(min_y)
+    max_y = max(y_vals)
+    max_y_index = y_vals.index(max_y)
+
+    max_y_x = x_vals[max_y_index]
+    min_y_x = x_vals[min_y_index]
+
+    tilted_left = False
+    # cube tilted slightly to the left, otherwise it's tilted slightly to the right
+    if max_y_x < min_y_x:
+        tilted_left = True
+
+    grid_points = [[[0,0] for x in range(3)] for y in range(3)]
+    sorted_y = sorted(points, key=lambda x: x[1])
+    sorted_x = sorted(points, key=lambda x: x[0])
+    if tilted_left:
+        grid_points[0][0] = sorted_x.pop(0)
+        grid_points[2][2] = sorted_x.pop(-1)
+        grid_points[2][0] = sorted_y.pop(0)
+        grid_points[0][2] = sorted_y.pop(-1)
+        grid_points[0][1] = sorted_x.pop(0)
+        grid_points[2][1] = sorted_x.pop(-1)
+        grid_points[1][0] = sorted_y.pop(0)
+        grid_points[1][2] = sorted_y.pop(-1)
+        for p in sorted_y:
+            if p not in grid_points[0] and p not in grid_points[1] and p not in grid_points[2]:
+                grid_points[1][1] = p
+                break
+    else:
+        grid_points[0][2] = sorted_x.pop(0)
+        grid_points[2][0] = sorted_x.pop(-1)
+        grid_points[0][0] = sorted_y.pop(0)
+        grid_points[2][2] = sorted_y.pop(-1)
+        grid_points[0][1] = sorted_x.pop(0)
+        grid_points[2][1] = sorted_x.pop(-1)
+        grid_points[1][0] = sorted_y.pop(0)
+        grid_points[1][2] = sorted_y.pop(-1)
+        for p in sorted_y:
+            if p not in grid_points[0] and p not in grid_points[1] and p not in grid_points[2]:
+                grid_points[1][1] = p
+                break
+    # check to make sure we have 9 unique points in the grid (if we don't then the
+    # tilted grid assumption must have been wrong and we will get errors when trying to
+    # calculate the residuals)
+    found_pts = set()
+    for col in grid_points:
+        for row in col:
+            found_pts.add(row)
+    if len(found_pts) != 9:
+        return float('inf')
+
+    # calculate the score as the residuals from a best fit line through each row and column
+    # TODO: account for how parallel the lines are to one another and whether they are the
+    # same distance apart from one another
+    grid_score = 0
+    for i in range(3):
+        x,y = zip(*grid_points[i])
+        _,resid,_,_,_ = np.polyfit(x,y,1,full=True)
+        if len(resid) == 1:
+            grid_score += resid
+    for j in range(3):
+        x = [grid_points[0][j][0], grid_points[1][j][0], grid_points[2][j][0]]
+        y = [grid_points[0][j][1], grid_points[1][j][1], grid_points[2][j][1]]
+        _,resid,_,_,_ = np.polyfit(x,y,1,full=True)
+        if len(resid) == 1:
+            grid_score += resid
+
+    return grid_score
+
+
+def straight_grid_score(points):    
+    cube_dim = 3
+    grid_points = [[[0,0] for x in range(3)] for y in range(3)] 
+    # try assuming a straight 3x3 cube
+    # group points by their y values
+    sorted_y = sorted(points, key=lambda x: x[1])
+    for i in range(cube_dim**2):
+        grid_points[i/cube_dim][i%cube_dim][0] = sorted_y[i][0]
+        grid_points[i/cube_dim][i%cube_dim][1] = sorted_y[i][1]
+    # sort each row of points by their x values
+    for i in range(cube_dim):
+        sorted_pts = sorted(copy.deepcopy(grid_points[i]), key=lambda x: x[0])
+        for j in range(cube_dim):
+            grid_points[i][j][0] = sorted_pts[j][0]
+            grid_points[i][j][1] = sorted_pts[j][1]
+
+    # TODO: account for whether lines are the same distance apart from one another
+    grid_score = 0
+    for i in range(cube_dim):
+        grid_score += np.std([grid_points[0][i][0], grid_points[1][i][0], grid_points[2][i][0]])
+    for j in range(cube_dim):
+        grid_score += np.std([grid_points[j][0][1], grid_points[j][1][1], grid_points[j][2][1]])
+        
+    
+    return grid_score
+
 def find_shapes(img):
     rows,cols,channels = img.shape
-    img_small = cv2.resize(cv2.blur(img, (3,3)), (cols/5, rows/5))
-    edges = auto_canny(img_small)
-    #cv2.imwrite('auto_edges.png', edges)
+    resize_factor = 4
+    # use a smaller version of the image
+    img_small = cv2.resize(img, (cols/resize_factor,rows/resize_factor), interpolation=cv2.INTER_AREA)
+    #img_small = cv2.resize(cv2.blur(img, (3,3)), (cols/5, rows/5), interpolation=cv2.INTER_AREA)
+    edges = auto_canny(img_small, 0.2)
     edges_dilated = cv2.dilate(edges, np.ones((5,5)), iterations=1)
     edges_dilated = 255-edges_dilated
-    #cv2.imwrite('auto_edges_dilated.png', edges)
+
+    # find contours, which should include squares from the cube faces
     contour_img, contours, hierarchy = cv2.findContours(edges_dilated.copy(),\
                             cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # draw the contours to help with debugging
     contour_img = img_small.copy()
     for contour in contours:
         color = (random.randint(0, 255), random.randint(0,255), random.randint(0,255))
         cv2.drawContours(contour_img, [contour], 0, color, 1)
-    #cv2.imwrite('contours.png', img_copy)
-    chosen_contour_img = img_small.copy()
-    center_img = np.zeros((rows/5, cols/5, 3))
+
+    squares_found = img_small.copy()
     centers = []
-    #start = time.time()
+    img_area = rows/resize_factor*cols/resize_factor
+    # for each contour check if the contour seems to be a square shape
     for contour in contours:
-        # perimiter to area ratio does a good job of detecting squares, we will
-        # want to find something else for detecting rectangles
-        if contour_is_square(contour):
-            color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
-            cv2.drawContours(chosen_contour_img, [contour], 0, color, -1)    
+        detected_square = find_bounding_square(contour, img_area)
+        if detected_square is not None:
             cX,cY = contour_center(contour)
+            cX,cY = cX,cY
             centers.append((cX,cY))
-            cv2.rectangle(center_img, (cX-2, cY-2), (cX+2, cY+2), (255,0,0), -1)
-            
-    #end = time.time()
-    #corners = pick_centers(centers)
-    #for corner in corners:
-    #    color = (random.randint(0, 255), random.randint(0,255), random.randint(0,255))
-    #    cv2.line(new_img, corner[0], corner[1], color, 1)
-    #    cv2.line(new_img, corner[1], corner[2], color, 1)
+            # draw the square shaped contours to help with debugging
+            color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+            cv2.drawContours(squares_found, [contour], 0, color, -1)
+
     
-    #centers2 = pick_centers2(centers)
-    #box = cv2.boxPoints(cv2.minAreaRect(np.array([[c] for c in centers2])))
-    #box = np.int0(box)
-    #cv2.drawContours(new_img,[box],0,(255,255,255),1) 
-    #cv2.imwrite('approximated_squares.png', img_copy)
-    #cv2.imwrite('approximated_centers.png', new_img)
-    return edges, edges_dilated, contour_img, chosen_contour_img, center_img
-    #print "took", end - start
+    # remove centers that aren't close to at least 3 other centers
+    pruned_centers = prune_centers(centers, cols/resize_factor)
+
+    # cluster the centers into groups where every center in each group is within X distance
+    # of the other centers in the group (this function is probably pretty inefficient right
+    # now so we should try to fix that)
+    grouped_centers = group_centers(pruned_centers, cols/resize_factor)
+    
+    chosen_contour_img = img_small.copy()
+    grid_scores = []
+    # in each of the groups of centers, look for a grid shape
+    # this current logic below assumes only 1 match will exist, we will want to adjust that
+    for gc in grouped_centers:
+        # we will want to find a way to handle more than 13-14 centers in way that doesn't
+        # exponentially explode, for now we simply don't try
+        if len(gc) >= 9 and len(gc) <= 14:
+            for c in itertools.combinations(gc, 9):
+                # we should adjust the grid score functions to also return the
+                # grid of points they detected so we don't have to reconstruct it
+                grid_scores.append([straight_grid_score(c), c])
+                grid_scores.append([tilted_grid_score(c), c])
+            grid_scores.sort()
+            if grid_scores[0][0] < 50:
+                for c in grid_scores[0][1]:
+                    color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+                    cv2.rectangle(chosen_contour_img, (c[0]-4, c[1]-4), (c[0]+4, c[1]+4), color, -1)
+
+    return cv2.resize(edges, (cols,rows)), cv2.resize(edges_dilated, (cols,rows)),\
+        cv2.resize(contour_img, (cols,rows)), cv2.resize(squares_found,(cols,rows)),\
+        cv2.resize(chosen_contour_img, (cols,rows))
 
 if __name__ == '__main__':
     runtimes = []
@@ -173,18 +385,28 @@ if __name__ == '__main__':
         for f in files:
             filename, ext = os.path.splitext(f)
             if ext == '.png':
-                os.mkdir(filename)
-                os.chdir(filename)
+                #os.mkdir(filename)
+                #os.chdir(filename)
                 print filename
-                img = cv2.imread(os.path.join('..',subdir,f))
+                img = cv2.imread(os.path.join(subdir,f))
                 start = time.time()
+                #images = find_lines(img)
                 images = find_shapes(img)
+                #images = lbp(img)
                 end = time.time()
                 print "took", end - start, "seconds"
                 runtimes.append(end-start)
-                cv2.imwrite('auto_edges.png', images[0])
-                cv2.imwrite('auto_edges_dilated.png', images[1])
-                cv2.imwrite('contour.png', images[2])
-                cv2.imwrite('approximated_squares.png', images[3])
-                cv2.imwrite('contour_centers.png', images[4])
-                os.chdir('..')
+                #cv2.imwrite('lbp'+filename+'.png', images)
+                #cv2.imwrite('auto_edges'+filename+'.png', images[0])
+                #cv2.imwrite('auto_edges_dilated'+filename+'.png', images[1])
+
+                """cv2.imwrite('detected_lines'+filename+'.png', images[0])
+                cv2.imwrite('adapt_thresh'+filename+'.png', images[1])
+                cv2.imwrite('laplacian'+filename+'.png', images[2])"""     
+                cv2.imwrite('auto_edges'+filename+'.png', images[0])
+                cv2.imwrite('auto_edges_dilated'+filename+'.png', images[1])
+                cv2.imwrite('contour'+filename+'.png', images[2])
+                cv2.imwrite('found_squares'+filename+'.png', images[3])
+                cv2.imwrite('approximated_squares'+filename+'.png', images[4])
+
+                #os.chdir('..')
---
+++ detect_cube_realtime.py
@@ -0,0 +1,24 @@
+import cv2,sys
+from detect_cube import find_shapes
+import numpy as np
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        cap = cv2.VideoCapture(sys.argv[1])
+    else:
+        cap = cv2.VideoCapture(0)
+        cv2.namedWindow('DISPLAY')
+
+    while cap.isOpened():
+	ret, image = cap.read()
+        rows,cols,channels = image.shape
+        images = find_shapes(image)
+	cv2.imshow('DISPLAY', cv2.resize(images[-1], (cols,rows)))
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.imwrite('test.png', image)
+	    break
+
+    cap.release()
+    cv2.destroyAllWindows()
