12de1dfea8167d4aa84d9860aa71f8661e71fd09
Andrew Scott <ascott@hmc.edu>
2016-11-27 21:54:21 -0800

    updating realtime code to use new find_shapes interface

--- detect_cube_realtime.py
+++ detect_cube_realtime.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 	ret, image = cap.read()
         rows,cols,channels = image.shape
         images = cube_detector.find_shapes(image)
-	cv2.imshow('DISPLAY', cv2.resize(images[-1], (cols,rows)))
+	cv2.imshow('DISPLAY', cv2.resize(images[1][-1], (cols,rows)))
 
 	if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.imwrite('test.png', image)
